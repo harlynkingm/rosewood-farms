57815b24c1ec045078aaeec646f60d5a2db6d25e
Max Harlynking <mharlynk@andrew.cmu.edu>
2017-07-01 12:44:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Rosewood Farms
+
+This is the official website for Rosewood Farms.
+
+Powered by React and Contentful.
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      </Router>
+    );
+  }
+}
+
+export default App;

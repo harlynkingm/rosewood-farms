3a55f692f971b3575017777dfbcee04294a846ec
Max Harlynking <mharlynk@andrew.cmu.edu>
2017-07-11 11:08:25 -0400

    changed routing type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {HashRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import {createClient} from 'contentful';
 import './css/normalize.css';
 import './css/skeleton.css';

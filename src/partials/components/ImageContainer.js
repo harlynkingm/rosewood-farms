2d229d7f3c1b94c560b87b8a700a959edb5cb06b
Max Harlynking <mharlynk@andrew.cmu.edu>
2017-07-13 15:55:09 -0400

    changed parallax speed

--- src/partials/components/ImageContainer.js
+++ src/partials/components/ImageContainer.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-parallax';
 
 export default class ImageContainer extends Component {
-  styleFromImage(image){
-    return {
-      backgroundImage: `url(${image})`
-    };
-  }
 
   render() {
     if (this.props.image){
       return (
-        <Parallax bgImage={this.props.image.fields.file.url} className="mainImage" bgHeight="100vh"/>
+        <Parallax bgImage={this.props.image.fields.file.url} className="mainImage" bgHeight="100vh" strength={400}/>
       );
     } else {
       return null;
